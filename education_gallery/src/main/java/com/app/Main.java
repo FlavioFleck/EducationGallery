fc237f16d4a129d09ff783091b656bc7c8152dd3
Flavio Fleck <flaviofleck55@gmail.com>
2024-10-31 13:04:45 -0300

    feat: Menu principal adicionado

--- education_gallery/src/main/java/com/app/Main.java
+++ education_gallery/src/main/java/com/app/Main.java
@@ -1,7 +1,33 @@
 package com.app;
+import java.util.Scanner;
+
 
 public class Main {
+    static Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("[1] - Usuário\n[2] - Academico \n[3] - Financeiro \n[4] - Instituição\n[5] - Sair");
+        System.out.print("Digite uma opção: ");
+        int option = input.nextInt();
+
+        switch (option) {
+            case 1:
+                System.out.println("\nUsuários");
+                break;
+            case 2:
+                System.out.println("\nAcademico");
+                break;
+            case 3:
+                System.out.println("\nFinanceiro");
+                break;
+            case 4:
+                System.out.println("\nInstituição");
+                break;
+            case 5:
+                System.exit(0);
+                break;
+            default:
+                break;
+        } 
     }
 }
